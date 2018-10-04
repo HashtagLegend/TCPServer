1dcb130810fd24249d545b21f4674227cea3c8af
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-10-04 21:47:56 +0200

    done

---
+++ TCPServer/EchoService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using Skat;
+
+namespace TCPServer
+{
+    class EchoService : Afgift
+    {
+        private TcpClient connectionSocket;
+
+        public EchoService(TcpClient connectionSocket)
+        {
+            this.connectionSocket = connectionSocket;
+        }
+
+        internal void DoIt()
+        {
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+
+            string message = sr.ReadLine();
+            string message2 = sr.ReadLine();
+            int answer;
+            
+            while (true)
+            {
+                if (message == "Bil")
+                {
+                    string pris = message2;
+                    Console.WriteLine("Bil");
+                    answer = BilAfgift(Convert.ToInt32(pris));
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+                }
+                else if (message == "Elbil")
+                {
+                    string pris = message2;
+                    Console.WriteLine("ElBil");
+                    answer = ElBilAfgift(Convert.ToInt32(pris));
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+                }
+
+            }
+            ns.Close();
+            connectionSocket.Close();
+        }
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 7000);
+            serverSocket.Start();
+
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Incoming connection established");
+                EchoService service = new EchoService(connectionSocket);
+                Task.Factory.StartNew(service.DoIt);
+            }
+
+            serverSocket.Stop();
+        }
+    }
+}
